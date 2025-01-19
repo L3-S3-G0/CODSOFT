6f88e6d3d6ed22a5567c52d4390397e87c12fdb4
L3-S3-G0 <mkglesego@gmail.com>
2025-01-19 20:11:10 +0200

    Print Statements
    Added the print statements for winning, getting a big or small number, as per condition in a while loop. And break condition.

--- NumberGame.java
+++ NumberGame.java
@@ -4,16 +4,44 @@ public class NumberGame{
     public static void main(String[] args) {
         Scanner inn = new Scanner(System.in);
         int guess;
-        int attempt;
+        int score = 0;
+        int games = 1;
+        int attempt = 5;
         int num = 44;
         boolean state = true;
+        String play;
 
         System.out.println("Let's play a number game!");
-        System.out.println("Guess a number between 1 and 100.");
         System.out.println("You have 5 attempts to guess the number.");
+        System.out.println("Guess a number between 1 and 100: ");
+
         
         guess = inn.nextInt();
         
-    }
+        while(state){
+           if( guess == num){
+             System.out.println("Congratulations. You have guessed correctly");
+             score++;
+           }
+           else if (guess > num){
+             attempt--;
+             System.out.println("Your guess is too big.");
+             System.out.println("Guess a number between 1 and 100: ");
+           }
+           else{
+             attempt--;
+             System.out.println("Your guess is too small.");
+             System.out.println("Guess a number between 1 and 100: ");
 
+           }
+           
+           if (attempt == 0){
+             System.out.println("You have run out of attempts.\nWould you like to play again? (Yes or No)");
+             play = inn.nextLine().toLowerCase();
+             
+             if (play.equals("no")){state = false; break;}
+             System.out.println("Guess a number between 1 and 100.");
+       
+        }   
+    }
 }
