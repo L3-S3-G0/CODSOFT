c8d3b2b5061026a475b65e2fda45cb3694be6412
L3-S3-G0 <mkglesego@gmail.com>
2025-01-22 19:06:17 +0200

    Wrapping up Task 2
    Wrote the main method to utilise the Report class and get user input. Also removed unnecessary variables created

--- Task 2/GradeCalculator.java
+++ Task 2/GradeCalculator.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class GradeCalculator{
  public static void main(String[] args){
- 
+  Scanner inn = new Scanner(System.in);
+  Report report = new Report();
+  
+  System.out.println("Please enter your grade\\s\nEnter DONE when finished ");
+  System.out.print("Grade: ");
+  String mark = inn.nextLine();
+  
+  while(true){
+   try{
+    double result = Double.parseDouble(mark);
+    int res = (int)result;
+    report.marksAdd(res);
+   }
+   catch(Exception e) {break;}
+   
+   System.out.print("Grade: ");
+   mark = inn.nextLine();
+  }
+  System.out.println();
+  report.displayResults();
  }
 }
--- Task 2/Report.java
+++ Task 2/Report.java
@@ -2,16 +2,12 @@ import java.util.ArrayList;
 
 public class Report{
 
- public ArrayList<String> subjects = new ArrayList<String>();
  public ArrayList<Integer> marks = new ArrayList<Integer>();
  public int[] grades_ = {1, 1, 1, 2, 3, 4, 5, 6, 7, 7 };
- public String[] grades = {"F", "F", "F", "F", "F", "D", "C", "B", "A", "A" };
+ public String[] grades = {"F", "F", "F", "F", "F", "D", "C", "B", "A", "A" , "A"};
+ 
+ public Report(){}
 
- 
- public void subjectAdd(String sub){
-   subjects.add(sub);
- }
- 
  public void marksAdd(int mark){
   marks.add(mark);
  }
