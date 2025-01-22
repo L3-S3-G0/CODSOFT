8c62e9676a65ab0e99466d36cbad60d0467df5a7
L3-S3-G0 <mkglesego@gmail.com>
2025-01-22 17:48:47 +0200

    Total Marks Calculator
    Wrote the total marks calculator by using a for-each loops in the arraylist holding the student's marks

--- Task 2/Report.java
+++ Task 2/Report.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
 public class Report{
-   
+
+ public ArrayList<String> subjects = new ArrayList<String>();
+ public ArrayList<Integer> marks = new ArrayList<Integer>();
+ 
+ public void subjectAdd(String sub){
+   subjects.add(sub);
+ }
+ 
+ public void marksAdd(int mark){
+  marks.add(mark);
+ }
+ 
+ public int totalMarks(){
+  int total = 0;
+  for (int i : marks){
+   total += i;
+  }
+  
+  return total;
+ }
+
 
 }
